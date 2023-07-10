7b54779e2d8ec6b03e51c662be8f3322c2a3615a
watti4 <emordi@ymail.com>
2023-07-10 22:34:55 +0100

    added new features and pages, expand and create more code lines too

---
+++ toggle.js
@@ -0,0 +1,10 @@
+  function toggleDivs() {
+    var monthly = document.getElementById("monthlySide");
+    var yearly = document.getElementById("yearlySide");
+   
+    monthlySide.classList.toggle("hidden");
+    monthlySide.classList.toggle("visible");
+    yearlySide.classList.toggle("hidden");
+    yearlySide.classList.toggle("visible");
+
+}   
